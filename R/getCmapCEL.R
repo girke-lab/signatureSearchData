5db66518e09af649dd2df0a9fea454ab45c56d49
Yuzhu Duan <yduan004@ucr.edu>
2021-12-06 12:26:37 -0800

    add dest_path to getCmapCEL

--- R/getCmapCEL.R
+++ R/getCmapCEL.R
@@ -8,6 +8,7 @@
 #' ExperimentHub instead.
 #' @title Download CMap2 CEL Files
 #' @param rerun TRUE or FALSE, whether to download the data
+#' @param dest_dir character(1), path to the destination directory
 #' @return download files
 #' @examples 
 #' getCmapCEL(rerun=FALSE) # set 'rerun' to TRUE if download
@@ -15,44 +16,25 @@
 #' @importFrom utils download.file
 #' @importFrom utils unzip
 #' @export
-getCmapCEL <- function(rerun=TRUE) {
+getCmapCEL <- function(rerun=TRUE, dest_dir=tempdir()) {
     if(rerun) {
         ## Check for presence of target directory
-        if(!dir.exists("./data/CEL"))
-            stop("Target directory ./data/CEL does not exist. 
-             Create it to run function.") 
+        if(!dir.exists(dest_dir))
+            stop(paste0("Target directory ", dest_dir, " does not exist. 
+             Create it to run function."))
+        dest_cel <- file.path(dest_dir, "CEL")
+        dir.create(dest_cel)
         ## Download CEL files
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume1of7.zip", 
-                      "./data/CEL/cmap_build02.volume1of7.zip")
-        unzip("./data/CEL/cmap_build02.volume1of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume1of7.zip")
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume2of7.zip", 
-                      "./data/CEL/cmap_build02.volume2of7.zip")
-        unzip("./data/CEL/cmap_build02.volume2of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume2of7.zip")
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume3of7.zip", 
-                      "./data/CEL/cmap_build02.volume3of7.zip")
-        unzip("./data/CEL/cmap_build02.volume3of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume3of7.zip")
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume4of7.zip", 
-                      "./data/CEL/cmap_build02.volume4of7.zip")
-        unzip("./data/CEL/cmap_build02.volume4of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume4of7.zip")
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume5of7.zip", 
-                      "./data/CEL/cmap_build02.volume5of7.zip")
-        unzip("./data/CEL/cmap_build02.volume5of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume5of7.zip")
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume6of7.zip", 
-                      "./data/CEL/cmap_build02.volume6of7.zip")
-        unzip("./data/CEL/cmap_build02.volume6of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume6of7.zip")
-        download.file("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume7of7.zip", 
-                      "./data/CEL/cmap_build02.volume7of7.zip")
-        unzip("./data/CEL/cmap_build02.volume7of7.zip", exdir="./data/CEL")
-        unlink("./data/CEL/cmap_build02.volume7of7.zip")
+        for(vol in 1:7){
+            dest_vol <- paste0(dest_cel, "/cmap_build02.volume", vol, "of7.zip")
+            download.file(paste0("ftp://ftp.broad.mit.edu/pub/cmap/cmap_build02.volume", vol, "of7.zip"), 
+                          dest_vol)
+            unzip(dest_vol, exdir=dest_cel)
+            unlink(dest_vol)
+        }
         
         ## Uncompress CEL files
-        myfiles <- list.files("./data/CEL", pattern=".CEL.bz2$", full.names=TRUE)
+        myfiles <- list.files(dest_cel, pattern=".CEL.bz2$", full.names=TRUE)
         for(i in myfiles) R.utils::bunzip2(i, remove=TRUE)
     } else {
         message("To execute function, set 'rerun=TRUE'")
