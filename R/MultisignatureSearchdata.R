85745115470bb2a7f18b0835655e22ff57fe6127
brendangongol <brengong@gmail.com>
2023-03-10 10:47:40 -0800

    added data.

---
+++ R/MultisignatureSearchdata.R
@@ -0,0 +1,7 @@
+#' SampleDEGdata
+#' @description A data table that contains sample differential gene expression data.
+#' @format A data table with 1001 rows and 73 columns.
+#' @examples
+#' data(SampleDEGdata)
+"SampleDEGdata"
+
